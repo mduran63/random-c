638a68f79e9e55311f42a7433979c9b114c59993
Mario Durán <mduran16@outlook.com>
2015-06-17 19:50:19 +0200

    random versión 2

--- Random.js
+++ Random.js
@@ -1,10 +1,10 @@
-// Math.random() devuelve número aleatorio entre 0 y 1. 
-var numero = Math.random(); 
+// Math.random() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
 
+var str = ' MAYOR que 0,5';
 
-if (numero <= 0.5) { 
-	console.log('\n' + numero + ' MENOR que 0,5 \n'); 
-} 
-else { 
-	console.log('\n' + numero + ' MAYOR que 0,5 \n');	 
-}; 
+if (numero <= 0.5) {
+	str = ' MENOR que 0,5';
+};
+
+console.log('\n' + numero + str + '\n');
